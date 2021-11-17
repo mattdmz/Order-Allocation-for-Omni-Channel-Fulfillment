746a75bcea8dc31e1c11fb1678b12703018ff209
mattdmz <matteo.demetz.md@gmail.com>
2021-11-17 14:14:44 +0100

    comments

--- experiment.py
+++ experiment.py
@@ -23,26 +23,26 @@ def main() -> None:
     allocation_period = timedelta(hours=3, minutes=0, seconds=0)
  
     # [LIST] of regions to allocated orders in, e.g. [1005]. All all regions should be considered tpye "all"                         
-    allocation_regions = [1006] #[1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008]
+    allocation_regions = [1001] #, 1002, 1003, 1004, 1005, 1006, 1007, 1008]
 
     # choose from allocator.constants, relevant for Longest_Stock_Duration
     
     experiment = Experiment(allocation_period, allocation_regions)
 
     # define number of days to test and number of times (periods) to run the test on consecutive days
-    test_days = 2
+    test_days = 31
     test_periods = 1
 
-    # set allocation methods to run program with (choices from allocation.rules)
-    allocation_methods = [Nearest_Nodes] #[Allocation_Of_Nearest_Order, Chepest_Direct_Delivery, Cheapest_Delivery, Dynamic_1, \
-                                #Longest_Stock_Duration, Modified_Dynamic_1, Nearest_Already_Allocated_Nodes, Nearest_Nodes, Operational_Costs]
+    # set allocation methods to run program with (choices from allocation.rules) as iterable (list)
+    allocation_methods = [Cheapest_Delivery] #[Chepest_Direct_Delivery, Cheapest_Delivery, Dynamic_1, Longest_Stock_Duration, Modified_Dynamic_1, 
+                          #Nearest_Already_Allocated_Nodes, Nearest_Nodes, Operational_Costs, Allocation_Of_Nearest_Order]
 
     # run experiment with defined parameters
     exp_runner = Experiment_Runner(test_days, test_periods, allocation_methods)
     exp_runner.run(experiment)
 
     # concat result csvs
-    # concat_results()
+    concat_results()
 
 if __name__ == "__main__":
 
