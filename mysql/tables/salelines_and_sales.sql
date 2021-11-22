5bb47b077927f337612d1f340a0782fd2a82700a
mattdmz <matteo.demetz.md@gmail.com>
2021-11-22 12:38:00 +0100

    updated script to upload sales data directly

--- mysql/tables/salelines_and_sales.sql
+++ mysql/tables/salelines_and_sales.sql
@@ -5,18 +5,193 @@ CREATE TABLE masterarbeit.sales(
 	node_id INT NOT NULL,
 	sales_date DATE,
 	sale_time TIME,
-	number_of_salelines SMALLINT NOT NULL,
 	price_str VARCHAR(7),
 	volume INT NOT NULL,
 	weight INT NOT NULL,
-    PRIMARY KEY (id),
 	its_datetime datetime,
+    PRIMARY KEY (id),
     FOREIGN KEY (node_id) REFERENCES nodes(id)
 );
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190301.csv'
 INTO TABLE masterarbeit.sales
-	FIELDS TERMINATED BY ';'
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190302.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190304.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190305.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190306.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190307.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190308.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190309.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190311.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_201903012.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190313.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190314.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190315.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190316.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190318.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190319.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190320.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190321.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_201903022.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190323.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190325.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190326.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190327.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190328.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190329.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
+           OPTIONALLY ENCLOSED BY '"'
+	LINES TERMINATED BY'\n'
+	IGNORE 1 ROWS;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sales_20190330.csv'
+INTO TABLE masterarbeit.sales
+	FIELDS TERMINATED BY ','
            OPTIONALLY ENCLOSED BY '"'
 	LINES TERMINATED BY'\n'
 	IGNORE 1 ROWS;
