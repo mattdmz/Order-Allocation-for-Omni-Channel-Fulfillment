ef955a5955a2cb53a691496d73780336fa9abd84
mattdmz <matteo.demetz.md@gmail.com>
2021-11-23 16:29:37 +0100

    removed while loop for db connection retry

--- database/connector.py
+++ database/connector.py
@@ -59,42 +59,30 @@ class Database:
 
         '''executes passed view'''
 
-        # set counter for trials
-        trials = 0
-
-        # try to connect max 2 times, else throw error
-        while trials <= 2:
-
-            try:
-                #excecute query with cursor of database
-                self.cursor.execute(view.sql)
-                
-                #fetch data from database according to query
-                data = self.cursor.fetchall()
-
-                #check if data could be fetched
-                if data == []:
-                    #report that query did not fetch any data
-                    raise NoDataError(view.name)
-                    
-                return data
+        try:
+            #excecute query with cursor of database
+            self.cursor.execute(view.sql)
             
-            except NoDataError as err:
-                pass #print(err)
-                return None
+            #fetch data from database according to query
+            data = self.cursor.fetchall()
 
-            except connector.Error as err:
-                # print error and info to its occurrence
-                err = (f"MySQL {err} \n"
-                       f"Error occured while runnning view '{view.name}' in database '{CONFIG[NAME]}' on trial: {trials}.") 
+            #check if data could be fetched
+            if data == []:
+                #report that query did not fetch any data
+                raise NoDataError(view.name)
                 
-                # sleep before retry
-                sleep(10)
-                trials += 1
+            return data
+        
+        except NoDataError as err:
+            pass #print(err)
+            return None
 
-            except connector.DatabaseError as err:
-                raise connector.DatabaseError(err)
+        except connector.Error as err:
+            # print error and info to its occurrence
+            err = (f"MySQL {err} \n"
+                    f"Error occured while runnning view '{view.name}' in database '{CONFIG[NAME]}' on trial: {trials}.") 
 
-        raise DatabaseError
+        except connector.DatabaseError as err:
+            raise connector.DatabaseError(err)
 
 
